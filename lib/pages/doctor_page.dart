55b138ad1ceaa9ec9fb1479b38f625a94fa6ef51
Georgemohsen <gogo.mohseen12@hotmail.com>
2025-05-16 00:33:28 +0300

    Finalizing Doctors Page

--- lib/pages/doctor_page.dart
+++ lib/pages/doctor_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_doctors_app/colors.dart';
+import 'package:flutter_doctors_app/pages/home_page.dart';
 
 class DoctorPage extends StatefulWidget {
   const DoctorPage({super.key});
@@ -9,12 +10,25 @@ class DoctorPage extends StatefulWidget {
 }
 
 class _DoctorPageState extends State<DoctorPage> {
+  bool clicked = false;
+  int? selectedIndex;
+  int? selectedDate;
+  int? selectedDate2;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: AppColors.primaryColor,
-        leading: Icon(Icons.arrow_back, color: Colors.white, size: 30),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          },
+          icon: Icon(Icons.arrow_back, size: 30),
+          color: Colors.white,
+        ),
         actions: [
           Padding(
             padding: const EdgeInsets.all(10.0),
@@ -27,6 +41,249 @@ class _DoctorPageState extends State<DoctorPage> {
         ],
       ),
       body: Column(
+        children: [
+          doctorHeader(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'April 2021',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "See all",
+                      style: TextStyle(fontSize: 18, color: Colors.grey),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                dayAppointment(),
+                SizedBox(height: 20),
+                morningAppointment(),
+                SizedBox(height: 20),
+                eveningAppointment(),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.all(16),
+        child: Row(
+          children: [
+            Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5), // Grey shadow color
+                    spreadRadius: 2, // How far the shadow spreads
+                    blurRadius: 5, // How soft the shadow is
+                    offset: Offset(2, 3), // Shadow position (x, y)
+                  ),
+                ],
+              ),
+              child: Icon(
+                Icons.favorite,
+                size: 30,
+                color: AppColors.primaryColor,
+              ),
+            ),
+            SizedBox(width: 10),
+            Expanded(
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.primaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.zero, // Sharp corners (fully rectangular)
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                ),
+                child: Text(
+                  'Book Appointment',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SizedBox dayAppointment() {
+    return SizedBox(
+      height: 80,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedIndex = (selectedIndex == index) ? null : index;
+              });
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color:
+                    selectedIndex == index
+                        ? AppColors.primaryColor
+                        : Colors.white,
+                shape: BoxShape.rectangle,
+              ),
+
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Sat',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color:
+                          selectedIndex == index ? Colors.white : Colors.black,
+                    ),
+                  ),
+                  Text(
+                    '16',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color:
+                          selectedIndex == index ? Colors.white : Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        separatorBuilder: (context, index) {
+          return SizedBox(width: 20);
+        },
+        itemCount: 7,
+      ),
+    );
+  }
+
+  Column eveningAppointment() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Evening',
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 10),
+        SizedBox(
+          height: 70,
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, // 3 columns
+              mainAxisSpacing: 5, // Vertical spacing between rows
+              crossAxisSpacing: 5, // Horizontal spacing between columns
+              childAspectRatio: 4, // Width/height ratio (adjust as needed)
+            ),
+            itemCount: 6, // 3 columns × 2 rows = 6 items
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selectedDate2 = index;
+                  });
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color:
+                        selectedDate2 == index
+                            ? AppColors.primaryColor
+                            : Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Text(
+                    '${9 + index}:00 AM', // Dynamic time (9AM, 10AM, etc.)
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color:
+                          selectedDate2 == index ? Colors.white : Colors.black,
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Column morningAppointment() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Morning',
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 10),
+        SizedBox(
+          height: 70,
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, // 3 columns
+              mainAxisSpacing: 5, // Vertical spacing between rows
+              crossAxisSpacing: 5, // Horizontal spacing between columns
+              childAspectRatio: 4, // Width/height ratio (adjust as needed)
+            ),
+            itemCount: 6, // 3 columns × 2 rows = 6 items
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selectedDate = index;
+                  });
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color:
+                        selectedDate == index
+                            ? AppColors.primaryColor
+                            : Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Text(
+                    '${9 + index}:00 AM', // Dynamic time (9AM, 10AM, etc.)
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color:
+                          selectedDate == index ? Colors.white : Colors.black,
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  SizedBox doctorHeader() {
+    return SizedBox(
+      child: Column(
         children: [
           Stack(
             //alignment: AlignmentDirectional.bottomEnd,
@@ -129,23 +386,8 @@ class _DoctorPageState extends State<DoctorPage> {
               ),
             ],
           ),
+
           SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'April 2021',
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  "See all",
-                  style: TextStyle(fontSize: 18, color: Colors.grey),
-                ),
-              ],
-            ),
-          ),
         ],
       ),
     );
